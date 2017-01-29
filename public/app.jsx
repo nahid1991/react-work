74f32040f7d91db9fa4d6f0b37f8e7f3586492da
Nahid <nahidshaiket10300@gmail.com>
2017-01-29 12:12:44 +0600

    first module

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,48 @@
+var GreeterMessage = React.createClass({
+  render: function(){
+    var name = this.props.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>Hello {name + '!!'}</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e){
+    e.preventDefault();
+
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+
+    if(name.length > 0){
+      this.refs.name.value = '';
+      updates.name = name;
+    }
+
+    if(message.length > 0){
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewData(updates);
+  },
+  render: function(){
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <input type="text" ref="name" placeholder="Enter Name"/><br/>
+        <textarea ref="message" placeholder="Enter message"></textarea><br/>
+        <button>Submit</button>
+      </form>
+    );
+  }
+});
+
+
 var Greeter = React.createClass({
   getDefaultProps: function(){
     return {
@@ -5,13 +50,25 @@ var Greeter = React.createClass({
       message: 'This is the default message'
     };
   },
+
+  handleNewData: function(updates){
+    this.setState(updates);
+  },
+
+  getInitialState: function(){
+    return {
+      name: this.props.name,
+      message: this.props.message
+    };
+  },
+
   render: function(){
-    var name = this.props.name;
-    var message = this.props.message;
+    var name = this.state.name;
+    var message = this.state.message;
     return (
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{message + '!!'}</p>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewData={this.handleNewData}/>
       </div>
     );
   }
