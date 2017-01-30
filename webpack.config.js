de720cb028ea785a77278451cc34bd96a6ceec70
Nahid <nahidshaiket10300@gmail.com>
2017-01-30 13:47:59 +0600

    webpack modified

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2017']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
